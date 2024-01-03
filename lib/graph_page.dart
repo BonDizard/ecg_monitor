b07b729aabc33309b5bb212f59b5c828e528732b
BonD <8123bharathbond8123@gmail.com>
2024-01-03 14:11:31 +0530

    sunil

--- lib/graph_page.dart
+++ lib/graph_page.dart
@@ -3,51 +3,60 @@ import 'package:flutter/material.dart';
 import 'package:chart_sparkline/chart_sparkline.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:lottie/lottie.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class GraphPage extends StatefulWidget {
-  const GraphPage({
-    super.key,
-  });
+  const GraphPage({Key? key}) : super(key: key);
 
   @override
   State<GraphPage> createState() => _GraphPageState();
 }
 
 class _GraphPageState extends State<GraphPage> {
-  List<double> data = [1];
-  final databaseReference = FirebaseDatabase.instance.ref().child('ecg/data');
+  List<double> data = [];
+  List<double> lastFiveData = [];
+  List<double> exactValues = [];
+  final databaseReference = FirebaseDatabase.instance.ref().child('ecg/sensor');
   late StreamSubscription subscription;
-  late Timer timer;
-  late Future<void> fetchDataFuture;
 
-  @override
-  void initState() {
-    super.initState();
-
-    fetchDataFuture = fetchData();
-
-    // Set up a timer to fetch data every 500ms
-    timer = Timer.periodic(const Duration(milliseconds: 500), (timer) {
-      fetchData();
-    });
+  // Function to normalize data between 0 and 1
+  double normalizeData(double value, double minValue, double maxValue) {
+    // Avoid division by zero
+    if (minValue == maxValue) {
+      return 0.0;
+    }
+    return (value - minValue) / (maxValue - minValue);
   }
 
   Future<void> fetchData() async {
-    // Fetch data from the database
     subscription = databaseReference.onValue.listen((event) {
       if (event.snapshot.value != null) {
-        // Update the UI with the fetched data
+        double newValue = (event.snapshot.value as num).toDouble();
+        exactValues.add(newValue);
+
         setState(() {
-          data.add((event.snapshot.value as num).toDouble());
+          data.add(newValue);
         });
+
+        // Keep only the last 10 values
+        if (data.length > 10) {
+          lastFiveData = data.sublist(data.length - 10);
+        } else {
+          lastFiveData = List.from(data);
+        }
       }
     });
   }
 
   @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  @override
   void dispose() {
-    // Cancel the timer and unsubscribe from the database when the widget is disposed
-    timer.cancel();
     subscription.cancel();
     super.dispose();
   }
@@ -65,28 +74,43 @@ class _GraphPageState extends State<GraphPage> {
         backgroundColor: Colors.purpleAccent,
       ),
       body: Center(
-        child: FutureBuilder(
-          future: fetchDataFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Lottie.asset(
-                  'assets/loading.json'); // Adjust the path based on your asset location
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            } else {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Center(
-                    child: Sparkline(
-                      key: UniqueKey(),
-                      data: data,
-                    ),
-                  ),
-                ],
-              );
-            }
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Sparkline(data: lastFiveData),
+            ),
+            Text('$lastFiveData'),
+            ElevatedButton(
+              onPressed: () async {
+                double minValue = exactValues.reduce((a, b) => a < b ? a : b);
+                double maxValue = exactValues.reduce((a, b) => a > b ? a : b);
+                // Normalize data between 0 and 1
+                List<double> normalizedData = data
+                    .map((value) => normalizeData(value, minValue, maxValue))
+                    .toList();
+                print('$normalizedData');
+                // Send the normalized data to the Flask server
+                final response = await http.post(
+                  Uri.parse(
+                      'http://localhost:5000/predict'), // Adjust the port if needed
+                  headers: {'Content-Type': 'application/json'},
+                  body: jsonEncode({'data': normalizedData}),
+                );
+
+                if (response.statusCode == 200) {
+                  // Handle the response from the Flask server
+                  var result = jsonDecode(response.body)['result'];
+                  print('Prediction result: $result');
+                } else {
+                  print('Failed to connect to the Flask server');
+                }
+                print('$normalizedData');
+              },
+              child: const Text('Predict'),
+            )
+          ],
         ),
       ),
     );
